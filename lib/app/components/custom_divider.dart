cbd8cc6480e9ff25ea10e462ac226bf2d0e58750
Anderson-Andre-P <eleu.ander@gmail.com>
2023-12-01 15:50:27 -0300

    feat: custom divider

---
+++ lib/app/components/custom_divider.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+
+import '../../config/translations/strings_enum.dart';
+
+class CustomDivider extends StatelessWidget {
+  const CustomDivider({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 16),
+      child: Row(
+        children: [
+          Expanded(
+            child: Divider(
+              color: Get.theme.dividerColor,
+              thickness: 1,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Text(
+              Strings.or.tr,
+              textAlign: TextAlign.center,
+              style: Get.theme.textTheme.displayMedium?.copyWith(
+                fontSize: 14.sp,
+                fontWeight: FontWeight.normal,
+              ),
+            ),
+          ),
+          Expanded(
+            child: Divider(
+              color: Get.theme.dividerColor,
+              thickness: 1,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
