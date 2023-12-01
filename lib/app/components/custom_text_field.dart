d20e9bac8e2711848ce6184d43e09ac3299a2eed
Anderson-Andre-P <eleu.ander@gmail.com>
2023-12-01 15:51:31 -0300

    feat: custom text field

--- lib/app/components/custom_text_field.dart
+++ lib/app/components/custom_text_field.dart
@@ -1,62 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
 
-class CustomTextField extends StatelessWidget {
+class CustomTextField extends StatefulWidget {
+  final String? label;
+  final IconData? prefixIcon;
+  final TextInputType keyboardType;
+  final bool isPassword;
+  final FormFieldValidator<String>? validator;
   final TextEditingController controller;
-  final String name;
-  final IconData prefixIcon;
-  final bool obscureText;
-  final TextCapitalization textCapitalization;
-  final TextInputType inputType;
-  final FormFieldValidator<String> validator;
 
   const CustomTextField({
     super.key,
+    required this.label,
+    this.keyboardType = TextInputType.text,
+    this.isPassword = false,
     required this.controller,
-    required this.name,
-    required this.prefixIcon,
-    this.obscureText = false,
-    this.textCapitalization = TextCapitalization.none,
-    required this.inputType,
-    required this.validator,
+    this.validator,
+    this.prefixIcon,
   });
 
   @override
+  // ignore: library_private_types_in_public_api
+  _CustomTextFieldState createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+  bool obscureText = false;
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(bottom: 15),
-      child: TextFormField(
-        validator: validator,
-        enabled: true,
-        controller: controller,
-        textCapitalization: textCapitalization,
-        maxLength: 32,
-        maxLines: 1,
-        obscureText: obscureText,
-        keyboardType: inputType,
-        textAlign: TextAlign.start,
-        style: const TextStyle(
-          color: Colors.black,
-          fontSize: 16,
+    return TextFormField(
+      key: widget.key, // Adicionando uma chave única
+      controller: widget.controller,
+      keyboardType: widget.keyboardType,
+      obscureText: obscureText,
+      validator: widget.validator,
+      decoration: InputDecoration(
+        prefixIcon: Icon(widget.prefixIcon),
+        suffixIcon: widget.isPassword
+            ? IconButton(
+                icon: Icon(
+                  obscureText ? Icons.visibility : Icons.visibility_off,
+                ),
+                onPressed: () {
+                  setState(() {
+                    obscureText = !obscureText;
+                  });
+                },
+              )
+            : null,
+        isDense: true,
+        labelText: widget.label ?? "labelText",
+        counterText: "",
+        labelStyle: const TextStyle(color: Colors.grey),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(4.r)),
         ),
-        decoration: InputDecoration(
-          prefixIcon: Icon(prefixIcon),
-          isDense: true,
-          labelText: name,
-          counterText: "",
-          labelStyle: const TextStyle(color: Colors.grey),
-          border: const OutlineInputBorder(
-            borderSide: BorderSide(color: Colors.green),
-            borderRadius: BorderRadius.all(Radius.circular(10)),
-          ),
-          focusedBorder: const OutlineInputBorder(
-            borderSide: BorderSide(color: Colors.green),
-            borderRadius: BorderRadius.all(Radius.circular(10)),
-          ),
-          enabledBorder: const OutlineInputBorder(
-            borderSide: BorderSide(color: Colors.green),
-            borderRadius: BorderRadius.all(Radius.circular(10)),
-          ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Get.theme.primaryColor),
+          borderRadius: BorderRadius.all(Radius.circular(4.r)),
         ),
+        // enabledBorder: OutlineInputBorder(
+        //   borderRadius: BorderRadius.all(Radius.circular(4.r)),
+        // ),
       ),
     );
   }
